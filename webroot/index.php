e492fc0e9f61bef6f6a4c343f25fe542cd3f57d1
Kevin Piacentini <kevinpiac@gmail.com>
2016-03-18 16:57:41 +0100

    Adding ShellRouter.php

--- webroot/index.php
+++ webroot/index.php
@@ -7,7 +7,7 @@ define('CORE', ROOT.DS.'core');
 define('CONFIG', ROOT.DS.'config');
 
 require(CORE.DS.'includes.php');
-
+/*
 $c = new Controller();
 $c->loadModel('Card');
 $params = array(
@@ -41,3 +41,17 @@ $c->Card->create();
 $c->Card->save(array("email" => "'test'", 'id' => '999999'));
 $res = $c->Card->findById(999999);
 print_r($res);
+*/
+
+$router = new ShellRouter($argc, $argv);
+print_r("Ctrl ->".$router->controller."\n");
+print_r("Action ->".$router->action."\n");
+foreach ($router->params as $param)
+{
+    print_r("Param ->".$param."\n");
+}
+
+$c = new Controller();
+$c->loadController($router->controller);
+
+$c->($router->action)();
