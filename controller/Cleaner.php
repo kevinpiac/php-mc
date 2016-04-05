665070fddab379f0a674dc3805d2dd43aa640e06
Kevin Piacentini <kevinpiac@gmail.com>
2016-04-06 00:51:58 +0200

    Save for backup

--- controller/Cleaner.php
+++ controller/Cleaner.php
@@ -7,16 +7,22 @@ class Cleaner extends Controller
     {
         $urls = [
             [
-                'url' => "https://graph.facebook.com/search?q=leserbe11%40gmx.fr&type=user&access_token=CAAAACZAVC6ygBALA3akfozB0fHa1c4OZBIa1eY77Ve6Xqg8qv68fZBfxXZA6Wnz1nK6ZBVw706expSanZAvqMIPmTcBIEZB38K8gmT4wHqP9ssGyJH5FT11czUXqzZCSSdx4Aq4W0IHpcsNcKwlQGtWPscwokAsN2f0okE0IIzC7ZA8HkN1YseNVc9eGUMyzTWIFoxnKddSU82gZDZD", 
-                'email' => 'moncul@gmail.com'
+                'url' => "https://graph.facebook.com/search?q=leserbe11%40gmx.fr&type=user&access_token=CAAAACZAVC6ygBALA3akfozB0fHa1c4OZBIa1ee6Xqg8qv68fZBfxXZA6Wnz1nK6ZBVw706expSanZAvqMIPmTcBIEZB38K8gmT4wHqP9ssGyJH5FT11czUXqzZCSSdx4Aq4W0IHpcsNcKwlQGtWPscwokAsN2f0okE0IIzC7ZA8HkN1YseNVc9eGUMyzTWIFoxnKddSU82gZDZD", 
+                'email' => 'wrongToken@gmail.com'
             ],
             [
-                'url' => "https://graph.facebook.com/search?q=kevinpiac@gmail.com&type=user&access_token=CAAAACZAVC6ygBALA3akfozB0fHa1c4OZBIa1eY77Ve6Xqg8qv68fZBfxXZA6Wnz1nK6ZBVw706expSanZAvqMIPmTcBIEZB38K8gmT4wHqP9ssGyJH5FT11czUXqzZCSSdx4Aq4W0IHpcsNcKwlQGtWPscwokAsN2f0okE0IIzC7ZA8HkN1YseNVc9eGUMyzTWIFoxnKddSU82gZDZD", 
-                'email' => 'kevinpiac@gmail.com'
+                'url' => "https://graph.facebook.com/search?q=wrong%40gmx.fr&type=user&access_token=CAAAACZAVC6ygBALA3akfozB0fHa1c4OZBIa1eY77Ve6Xqg8qv68fZBfxXZA6Wnz1nK6ZBVw706expSanZAvqMIPmTcBIEZB38K8gmT4wHqP9ssGyJH5FT11czUXqzZCSSdx4Aq4W0IHpcsNcKwlQGtWPscwokAsN2f0okE0IIzC7ZA8HkN1YseNVc9eGUMyzTWIFoxnKddSU82gZDZD", 
+                'email' => 'wrongEmail@gmail.com'
+            ],
+            [
+                'url' => "https://graph.facebook.com/search?q=leserbe11%40gmx.fr&type=user&access_token=CAAAACZAVC6ygBALA3akfozB0fHa1c4OZBIa1eY77Ve6Xqg8qv68fZBfxXZA6Wnz1nK6ZBVw706expSanZAvqMIPmTcBIEZB38K8gmT4wHqP9ssGyJH5FT11czUXqzZCSSdx4Aq4W0IHpcsNcKwlQGtWPscwokAsN2f0okE0IIzC7ZA8HkN1YseNVc9eGUMyzTWIFoxnKddSU82gZDZD", 
+                'email' => 'GoodEmailAndToken@gmail.com'
             ]
         ];
         return $urls;
     }
+
+    // voir pour traiter la blank page error !
 
     public function getFacebookIds()
     {
@@ -25,20 +31,52 @@ class Cleaner extends Controller
         $urls = $this->getEmailsToClean();
         require ROOT.'/controller/Curl.php';
         $res = Curl::CurlOpenGraph($urls, $proxy);
-   print_r($res);
-        $res = $this->checkFacebookResult($res);
-        print_r($res);
-        return ($res);
+
+        return ($this->handleFacebookResults($res));
     }
 
-    public function checkFacebookResult($result)
+    public function getFacebookDataByIds()
     {
-        $res = [];
+        
+    }
 
+    public function handleFacebookResults($result)
+    {
+        $verified = [];
+        $token_errors = [];
+        $email_errors = [];
+
+        // on divise le resultat en trois tableaux
         foreach ($result as $k => $v)
         {
-            if (!empty($v['id']))
-                array_push($res, $k);
+            if (isset($v['email_error']))
+                array_push($email_errors, $v);
+            else if(isset($v['token_error']))
+                array_push($token_errors, $v);
+            else if(!empty($v['id']))
+                array_push($verified, $v);
         }
+
+        // Only for debug.
+        $this->debug = true;
+        if (isset($this->debug))
+        {
+            print_r($verified);
+            print_r($token_errors);
+            print_r($email_errors);
+            
+            $tk_err_count = count($token_errors);
+            $mail_err_count = count($email_errors);
+            $verif_count = count($verified);
+            
+            print_r("\n token errors: ". $tk_err_count);
+            print_r("\n email errors: ". $mail_err_count);
+            print_r("\n succes      : ". $verif_count."\n");
+        }
+
+        // On traite chacun des tableaux
+        
+        //        $this->getFacebookDataByIds($verifieds);
+        //$this->handleTokenError($token_errors);
     }
 } 
